7523ae296a7036e1d84388a22c087a99cf91bc26
Lucas de Abreu dos Santos <lukas.abreu1056@gmail.com>
2021-06-15 15:18:27 -0300

    Alteração Layout
    Alterações na tela de Login..

--- ULogin.pas
+++ ULogin.pas
@@ -4,11 +4,14 @@ interface
 
 uses
   System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants,
-  FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs, FMX.Layouts;
+  FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs, FMX.Layouts,
+  FMX.Objects;
 
 type
   TFrmLogin = class(TForm)
     Layout1: TLayout;
+    Layout3: TLayout;
+    Image1: TImage;
   private
     { Private declarations }
   public
