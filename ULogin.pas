61268764bd3b26418da940b81864d7eeb63a05f3
Lucas de Abreu dos Santos <lukas.abreu1056@gmail.com>
2021-06-16 08:23:11 -0300

    Alterações
    Alteração basica

--- ULogin.pas
+++ ULogin.pas
@@ -5,13 +5,16 @@ interface
 uses
   System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants,
   FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs, FMX.Layouts,
-  FMX.Objects;
+  FMX.Objects, FMX.Controls.Presentation, FMX.Edit;
 
 type
   TFrmLogin = class(TForm)
     Layout1: TLayout;
     Layout3: TLayout;
     Image1: TImage;
+    Layout2: TLayout;
+    RoundRect1: TRoundRect;
+    Edit1: TEdit;
   private
     { Private declarations }
   public
