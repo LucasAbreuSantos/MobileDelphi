4e7542b2961300b3482179be8a2fdfe6fecbed8b
Lucas de Abreu dos Santos <lukas.abreu1056@gmail.com>
2021-06-22 15:13:27 -0300

    Alteração
    Alterações de Navegação

--- Financeiro.dpr
+++ Financeiro.dpr
@@ -3,7 +3,8 @@ program Financeiro;
 uses
   System.StartUpCopy,
   FMX.Forms,
-  ULogin in 'ULogin.pas' {FrmLogin};
+  ULogin in 'ULogin.pas' {FrmLogin},
+  u99Permissions in 'Units\u99Permissions.pas';
 
 {$R *.res}
 
